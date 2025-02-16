ffa3ab2171534de636065d940c8bb82076e48456
root.Quantum <mr.astro.314@gmail.com>
2025-02-16 14:05:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let no = document.querySelector('.no_button');
+        if(press == 0){
+            no.addEventListener("click", function(){
+                document.getElementById("word").innerHTML = "I THING YOU PRESSED WRONG BUTTON... TRY AGAIN";
+                document.getElementById('image').src = 'cats-miskey-the-peacemaker.gif';
+            })
+        } else if (press == 2) {
+            no.addEventListener("click", function(){
+                
+            })
+        }
+        
